46ecd0b97eaff12e58a409eef688fd8ef5039b74
Lillian Violet <git@gladtherescake.eu>
2023-10-23 17:32:58 +0200

    Unstable versions bc I'm cutting me on that edge

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
@@ -11,7 +11,7 @@
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Add any other flake you might need
--- home-manager/EDI-Lillian.nix
+++ home-manager/EDI-Lillian.nix
@@ -18,7 +18,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
-  ];
+  ];23.05
 
   nixpkgs = {
     # You can add overlays here
@@ -192,5 +192,5 @@
   systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "23.05";
+  home.stateVersion = "23.11";
 }
