816b858c75abb7b5b8d2fe204b0152e1ea8c50f1
Lillian-Violet <git@gladtherescake.eu>
2023-10-29 21:19:51 +0100

    Sops don't work yet, updated lock, zsh nix shell

--- home-manager/EDI-Lillian.nix
+++ home-manager/EDI-Lillian.nix
@@ -118,6 +118,18 @@
 
   programs.zsh = {
     enable = true;
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.7.0";
+          sha256 = "149zh2rm59blr2q458a5irkfh82y3dwdich60s9670kl3cl5h2m1";
+        };
+      }
+    ];
     enableAutosuggestions = true;
     enableCompletion = true;
     historySubstringSearch.enable = true;
@@ -182,7 +194,6 @@
         "web-search"
         "zsh-interactive-cd"
         "zsh-navigation-tools"
-        "zsh-nix-shell"
       ];
       theme = "jtriley";
     };
