4f784e56821cb1ff8d069aa7d2429c86ba698cf9
Lillian-Violet <git@gladtherescake.eu>
2023-11-12 18:50:59 +0100

    Confgure steam correctly

--- home-manager/GLaDOS-Lillian.nix
+++ home-manager/GLaDOS-Lillian.nix
@@ -101,6 +101,13 @@
     ungoogled-chromium
   ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+  hardware.opengl.driSupport32Bit = true; # Enables support for 32bit libs that steam usess
+
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
