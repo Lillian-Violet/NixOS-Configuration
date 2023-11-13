16a270f0605bb12123654466191440290483ec87
Lillian-Violet <git@gladtherescake.eu>
2023-11-13 21:28:22 +0100

    Relative paths my beloathed

--- nixos/GLaDOS/configuration.nix
+++ nixos/GLaDOS/configuration.nix
@@ -22,7 +22,7 @@
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
 
-    ././shared-desktop/configuration.nix
+    .././shared-desktop/configuration.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
