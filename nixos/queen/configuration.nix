cfd200ca2f8f1dab217b055b23f1cc635830a18f
Lillian-Violet <git@gladtherescake.eu>
2023-11-23 14:35:01 +0100

    Let's try this found config

--- nixos/queen/configuration.nix
+++ nixos/queen/configuration.nix
@@ -53,9 +53,7 @@
   #Set up sops config, and configure where the keyfile is, then set the mode for the unencrypted keys
   sops.defaultSopsFile = ../../secrets/queen-Lillian.yaml;
   sops.age.keyFile = ./keys.txt;
-  sops.secrets."nextcloudadmin".mode = "0400";
-  sops.secrets."nextcloudadmin".owner = config.users.users.nextcloud.name;
-  sops.secrets."nextclouddb".mode = "0400";
+  sops.secrets."nextclouddb".mode = "0440";
   sops.secrets."nextclouddb".owner = config.users.users.nextcloud.name;
 
   nix = {
