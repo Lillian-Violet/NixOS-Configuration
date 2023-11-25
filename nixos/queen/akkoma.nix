7846dceb8ae35029734d8f5e908ca809552975c9
Lillian-Violet <git@lillianviolet.dev>
2023-11-25 14:49:56 +0100

    Setup db

--- nixos/queen/akkoma.nix
+++ nixos/queen/akkoma.nix
@@ -29,9 +29,16 @@
         name = "GLaDTheresCake Akkoma";
         email = "akkoma@gladtherescake.eu";
         notify_email = "no-reply@akkoma.gladtherescake.eu";
+        emails.mailer = {
+          enabled = true;
+          adapter = "Swoosh.Adapters.Sendmail";
+          cmd_path = "/run/wrappers/bin/sendmail";
+          cmd_args = "-N delay,failure,success";
+          qmail = true;
+        };
         description = "Lillian's Akkoma server!";
         languages = ["en" "nl"];
-        registrations_open = true;
+        registrations_open = false;
         max_pinned_statuses = 10;
         cleanup_attachments = true;
       };
