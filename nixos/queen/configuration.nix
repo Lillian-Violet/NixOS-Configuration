47a68d148760067f031cffa90d1928d772cc34e5
Lillian-Violet <git@lillianviolet.dev>
2023-11-26 19:27:58 +0100

    Oops

--- nixos/queen/configuration.nix
+++ nixos/queen/configuration.nix
@@ -208,5 +208,5 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "unstable";
+  system.stateVersion = "23.05";
 }
