4f324b9bcb7ac3fce4f405589a82585ce4684656
Lillian-Violet <git@gladtherescake.eu>
2023-11-26 19:18:09 +0100

    Try this?

--- nixos/queen/nextcloud.nix
+++ nixos/queen/nextcloud.nix
@@ -93,7 +93,7 @@
   services.postgresql = {
     enable = true;
 
-    package = pkgs.postgresql_15;
+    package = pkgs.postgresql;
 
     # Ensure the database, user, and permissions always exist
     ensureDatabases = ["nextcloud" "onlyoffice" "akkoma"];
