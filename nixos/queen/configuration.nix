33542b6eb190b975e39a042f21cccfa3eaf55498
Lillian-Violet <git@lillianviolet.dev>
2023-11-26 18:25:14 +0100

    Update to unstabel to see if that fixes things

--- nixos/queen/configuration.nix
+++ nixos/queen/configuration.nix
@@ -208,5 +208,5 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "23.05";
+  system.stateVersion = "unstable";
 }
