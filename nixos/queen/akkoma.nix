1972d0c8804bd23a91863a2ba0db13b9acdca54a
Lillian-Violet <git@gladtherescake.eu>
2023-11-27 12:10:44 +0100

    Try and run akkoma default and edit with ctl

--- nixos/queen/akkoma.nix
+++ nixos/queen/akkoma.nix
@@ -25,25 +25,25 @@
       serverName = "akkoma.gladtherescake.eu";
     };
     #dist.cookie._secret = config.sops.secrets."releaseCookie".path;
-
-    config = {
-      ":pleroma".":instance" = {
-        name = "GLaDTheresCake Akkoma";
-        email = "akkoma@gladtherescake.eu";
-        notify_email = "no-reply@akkoma.gladtherescake.eu";
-        emails.mailer = {
-          enabled = true;
-          adapter = "Swoosh.Adapters.Sendmail";
-          cmd_path = "/run/wrappers/bin/sendmail";
-          cmd_args = "-N delay,failure,success";
-          qmail = true;
-        };
-        description = "Lillian's Akkoma server!";
-        languages = ["en" "nl"];
-        registrations_open = true;
-        max_pinned_statuses = 10;
-        cleanup_attachments = true;
-      };
-    };
+    config = {};
+    # config = {
+    #   ":pleroma".":instance" = {
+    #     name = "GLaDTheresCake Akkoma";
+    #     email = "akkoma@gladtherescake.eu";
+    #     notify_email = "no-reply@akkoma.gladtherescake.eu";
+    #     emails.mailer = {
+    #       enabled = true;
+    #       adapter = "Swoosh.Adapters.Sendmail";
+    #       cmd_path = "sendmail";
+    #       cmd_args = "-N delay,failure,success";
+    #       qmail = true;
+    #     };
+    #     description = "Lillian's Akkoma server!";
+    #     languages = ["en" "nl"];
+    #     registrations_open = true;
+    #     max_pinned_statuses = 10;
+    #     cleanup_attachments = true;
+    #   };
+    # };
   };
 }
