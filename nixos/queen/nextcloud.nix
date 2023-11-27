7fa997ec94533c5bbff088a084179c40e9bd948b
Lillian-Violet <git@lillianviolet.dev>
2023-11-27 15:06:57 +0100

    Let's configure the db for gotosocial?

--- nixos/queen/nextcloud.nix
+++ nixos/queen/nextcloud.nix
@@ -105,28 +105,6 @@
     enable = true;
   };
 
-  # Enable PostgreSQL
-  services.postgresql = {
-    enable = true;
-
-    # Ensure the database, user, and permissions always exist
-    ensureDatabases = ["nextcloud" "onlyoffice" "akkoma"];
-    ensureUsers = [
-      {
-        name = "nextcloud";
-        ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
-      }
-      {
-        name = "onlyoffice";
-        ensurePermissions."DATABASE onlyoffice" = "ALL PRIVILEGES";
-      }
-      {
-        name = "akkoma";
-        ensurePermissions."DATABASE akkoma" = "ALL PRIVILEGES";
-      }
-    ];
-  };
-
   services.aria2 = {
     enable = true;
     rpcListenPort = 6969;
