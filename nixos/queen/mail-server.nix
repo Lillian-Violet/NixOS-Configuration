588161a0c6071482b9e6f176b918fa224c1ed307
Lillian-Violet <git@gladtherescake.eu>
2023-12-15 21:57:14 +0100

    Let's try two servers?

--- nixos/queen/mail-server.nix
+++ nixos/queen/mail-server.nix
@@ -36,7 +36,6 @@
       "nextcloud.gladtherescake.eu"
       "akkoma.gladtherescake.eu"
       "social.gladtherescake.eu"
-      "lillianviolet.dev"
     ];
 
     loginAccounts = {
@@ -49,6 +48,26 @@
       "no-reply@social.gladtherescake.eu" = {
         hashedPasswordFile = config.sops.secrets."mailpass".path;
       };
+    };
+
+    rejectRecipients = [
+      "no-reply@nextcloud.gladtherescake.eu"
+      "no-reply@akkoma.gladtherescake.eu"
+      "no-reply@social.gladtherescake.eu"
+    ];
+    certificateScheme = "acme-nginx";
+  };
+
+  mailserver = {
+    enable = true;
+    enableImap = true;
+    enableSubmission = true;
+    fqdn = "mail.lillianviolet.dev";
+    domains = [
+      "lillianviolet.dev"
+    ];
+
+    loginAccounts = {
       "info@lillianviolet.dev" = {
         hashedPasswordFile = config.sops.secrets."mailpass".path;
         aliases = [
@@ -59,12 +78,6 @@
         ];
       };
     };
-
-    rejectRecipients = [
-      "no-reply@nextcloud.gladtherescake.eu"
-      "no-reply@akkoma.gladtherescake.eu"
-      "no-reply@social.gladtherescake.eu"
-    ];
     certificateScheme = "acme-nginx";
   };
 }
