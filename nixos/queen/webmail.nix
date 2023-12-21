dd523915632d8d6dfd1cddea770b255c8b5e1bc0
Lillian-Violet <git@lillianviolet.dev>
2023-12-21 21:15:44 +0100

    Change domain webmail

--- nixos/queen/webmail.nix
+++ nixos/queen/webmail.nix
@@ -8,7 +8,7 @@
     enable = true;
     # this is the url of the vhost, not necessarily the same as the fqdn of
     # the mailserver
-    hostName = "mail.lillianviolet.dev";
+    hostName = "webmail.lillianviolet.dev";
     extraConfig = ''
       # starttls needed for authentication, so the fqdn required to match
       # the certificate
