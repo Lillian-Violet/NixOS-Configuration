6a4ef2685dd2f609e49e202a07749d649074d911
Lillian-Violet <git@lillianviolet.dev>
2023-12-30 20:14:00 +0100

    Enable home manager on deck

--- home-manager/hosts/shodan/shodan-Lillian.nix
+++ home-manager/hosts/shodan/shodan-Lillian.nix
@@ -20,6 +20,37 @@
     # ./nvim.nix
     ../../package-configs/zsh.nix
   ];
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "lillian";
+    homeDirectory = "/home/lillian";
+  };
 
   home.packages = with pkgs; [
     #Chat:
@@ -50,6 +81,14 @@
     librewolf
     ungoogled-chromium
   ];
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  programs.git = {
+    enable = true;
+    userEmail = "git@gladtherescake.eu";
+    userName = "Lillian-Violet";
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.11";
