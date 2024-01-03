99d7e48a9747b11c3061e770645decce6a0113f4
Lillian-Violet <git@gladtherescake.eu>
2024-01-03 23:13:11 +0100

    host radarr and sonarr online

--- nixos/package-configs/radarr/configuration.nix
+++ nixos/package-configs/radarr/configuration.nix
@@ -7,4 +7,16 @@
   services.radarr = {
     enable = true;
   };
+
+  services.nginx = {
+    virtualHosts = {
+      "radarr.gladtherescake.eu" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://localhost:7878";
+        };
+      };
+    };
+  };
 }
