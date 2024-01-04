8605bfff925a0211500cb6a9f48489418ab8a380
Lillian-Violet <git@lillianviolet.dev>
2024-01-04 14:52:39 +0100

    Doesn't seem to be a user anyway

--- nixos/server/package-configs/ombi/configuration.nix
+++ nixos/server/package-configs/ombi/configuration.nix
@@ -14,10 +14,6 @@
   users.users = {
     radarr.extraGroups = ["aria2"];
     sonarr.extraGroups = ["aria2"];
-    prowlarr = {
-      group = "prowlarr";
-      extraGroups = ["aria2"];
-    };
   };
   #uses port 7878
   services.radarr = {
