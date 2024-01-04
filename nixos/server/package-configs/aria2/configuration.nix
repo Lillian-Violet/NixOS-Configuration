d5f7649b88985365d5f7f1b5614a33178ad7b202
Lillian-Violet <git@lillianviolet.dev>
2024-01-04 23:25:08 +0100

    Add ipv6 address

--- nixos/server/package-configs/aria2/configuration.nix
+++ nixos/server/package-configs/aria2/configuration.nix
@@ -31,6 +31,8 @@
     privateNetwork = true;
     hostAddress = "192.168.100.10";
     localAddress = "192.168.100.11";
+    hostAddress6 = "fc00::1";
+    localAddress6 = "fc00::2";
     config = {
       config,
       pkgs,
