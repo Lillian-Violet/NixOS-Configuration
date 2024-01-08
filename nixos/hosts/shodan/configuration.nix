50a2a850ba0aa0d1a8b77711c75a2221f577b687
Lillian-Violet <git@lillianviolet.dev>
2024-01-08 22:48:12 +0100

    Removed references to the overlay and pkgs folder

--- nixos/hosts/shodan/configuration.nix
+++ nixos/hosts/shodan/configuration.nix
@@ -39,10 +39,6 @@
   nixpkgs = {
     # You can add overlays here
     overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
       inputs.extest.overlays.default
 
       # You can also add overlays exported from other flakes:
