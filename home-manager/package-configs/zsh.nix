32379262c98452150844eadfcce0c2f95770cb24
Lillian-Violet <git@lillianviolet.dev>
2024-01-08 23:48:14 +0100

    Update zsh-nix-shell version

--- home-manager/package-configs/zsh.nix
+++ home-manager/package-configs/zsh.nix
@@ -15,7 +15,7 @@
         src = pkgs.fetchFromGitHub {
           owner = "chisui";
           repo = "zsh-nix-shell";
-          rev = "v0.7.0";
+          rev = "v0.8.0";
           sha256 = "149zh2rm59blr2q458a5irkfh82y3dwdich60s9670kl3cl5h2m1";
         };
       }
