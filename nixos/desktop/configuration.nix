808ab9fea5169f6d587aff05fd3e17e6fb56c336
Lillian-Violet <git@lillianviolet.dev>
2024-01-09 13:19:42 +0100

    Small refactor to make locale a global setting

--- nixos/desktop/configuration.nix
+++ nixos/desktop/configuration.nix
@@ -8,6 +8,9 @@
   pkgs,
   ...
 }: {
+  imports = [
+    ../shared/locale/configuration.nix
+  ];
   nixpkgs = {
     # You can add overlays here
     overlays = [
@@ -100,21 +103,6 @@
 
   # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "nl_NL.UTF-8";
-    LC_IDENTIFICATION = "nl_NL.UTF-8";
-    LC_MEASUREMENT = "nl_NL.UTF-8";
-    LC_MONETARY = "nl_NL.UTF-8";
-    LC_NAME = "nl_NL.UTF-8";
-    LC_NUMERIC = "nl_NL.UTF-8";
-    LC_PAPER = "nl_NL.UTF-8";
-    LC_TELEPHONE = "nl_NL.UTF-8";
-    LC_TIME = "en_DK.UTF-8";
-  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
