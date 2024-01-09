358c68416a14d1abb62f5b0f5745d1781a5fcf83
Lillian-Violet <git@lillianviolet.dev>
2024-01-09 15:48:49 +0100

    Add openvscode to queen

---
+++ nixos/server/package-configs/vscode-server/configuration.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  services.openvscode = {
+    enable = true;
+    port = 7773;
+    telemetryLevel = "off";
+    withoutConnectionToken = true;
+  };
+  services.nginx = {
+    virtualHosts = {
+      "code.lillianviolet.dev" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://localhost:7773";
+        };
+      };
+    };
+  };
+}
