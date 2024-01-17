0517723e784a04862d8dad1b9e8582179d0ca23e
Lillian-Violet <git@lillianviolet.dev>
2024-01-17 10:31:08 +0100

    Need this since I don't feel like sorting this rn

--- flake.nix
+++ flake.nix
@@ -63,8 +63,6 @@
           sops-nix.nixosModules.sops
           home-manager.nixosModules.home-manager
           {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
             home-manager.sharedModules = [plasma-manager.homeManagerModules.plasma-manager];
           }
         ];
@@ -81,8 +79,6 @@
           sops-nix.nixosModules.sops
           home-manager.nixosModules.home-manager
           {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
             home-manager.sharedModules = [plasma-manager.homeManagerModules.plasma-manager];
           }
         ];
