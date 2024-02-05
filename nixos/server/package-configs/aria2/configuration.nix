5fb3e5ed7a74e4dcdf40deca8ec17eea96cdc03a
Lillian-Violet <git@lillianviolet.dev>
2024-02-05 18:23:09 +0100

    Update configs to fix updates, and some minor tweaks

--- nixos/server/package-configs/aria2/configuration.nix
+++ nixos/server/package-configs/aria2/configuration.nix
@@ -1,4 +1,6 @@
 {
+  inputs,
+  outputs,
   config,
   pkgs,
   ...
@@ -7,9 +9,13 @@
   users.groups.aria2 = {};
   users.users.aria2.isSystemUser = true;
 
+  sops.secrets."rpcSecret".mode = "0440";
+  sops.secrets."rpcSecret".owner = config.users.users.aria2.name;
+
   services.aria2 = {
     enable = true;
     downloadDir = "/var/lib/media";
     rpcListenPort = 6969;
+    rpcSecretFile = config.sops.secrets."rpcSecret".path;
   };
 }
