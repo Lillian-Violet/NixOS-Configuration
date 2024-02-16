cf619a9a4da21205da84f69487e51413d199f8e0
Lillian-Violet <git@lillianviolet.dev>
2024-02-16 12:21:34 +0100

    Add some obs plugins to GLaDOS

--- home-manager/hosts/GLaDOS/default.nix
+++ home-manager/hosts/GLaDOS/default.nix
@@ -10,4 +10,12 @@
   imports = [
     ./lillian.nix
   ];
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+  };
 }
