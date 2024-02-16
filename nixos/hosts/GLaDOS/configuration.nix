f52e9eddd41483050365889f3e945b563d1a5cb1
Lillian-Violet <git@lillianviolet.dev>
2024-02-16 10:45:49 +0100

    Or this?

--- nixos/hosts/GLaDOS/configuration.nix
+++ nixos/hosts/GLaDOS/configuration.nix
@@ -46,7 +46,20 @@
   boot.loader.systemd-boot.configurationLimit = 3;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = ["bcachefs"];
-  boot.kernelModules = ["hid-nintendo" "v4l2loopback"];
+  boot.extraModulePackages = with config.boot.kernelPackages; [v4l2loopback.out];
+  boot.kernelModules = [
+    # Virtual Camera
+    "v4l2loopback"
+    # Virtual Microphone, built-in
+    "snd-aloop"
+  ];
+  # Set initial kernel module settings
+  boot.extraModprobeConfig = ''
+    # exclusive_caps: Skype, Zoom, Teams etc. will only show device when actually streaming
+    # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
+    # https://github.com/umlaeute/v4l2loopback
+    options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+  '';
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   home-manager = {
