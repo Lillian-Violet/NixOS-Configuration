f9a34d7f3ba1828b5a14e45f4ca424a5ec401cd2
Lillian-Violet <git@lillianviolet.dev>
2024-02-16 12:23:46 +0100

    Remove obs from GLaDOS specific and configure it in desktop instead

--- home-manager/hosts/GLaDOS/default.nix
+++ home-manager/hosts/GLaDOS/default.nix
@@ -10,12 +10,4 @@
   imports = [
     ./lillian.nix
   ];
-  programs.obs-studio = {
-    enable = true;
-    plugins = with pkgs.obs-studio-plugins; [
-      wlrobs
-      obs-backgroundremoval
-      obs-pipewire-audio-capture
-    ];
-  };
 }
