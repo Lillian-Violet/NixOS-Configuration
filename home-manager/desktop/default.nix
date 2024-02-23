17f9ea4ffc15780b647a9746c3672291b73cf450
Lillian-Violet <git@lillianviolet.dev>
2024-02-23 14:04:42 +0100

    Let's disable it and see if I can dynamically add it with direnv

--- home-manager/desktop/default.nix
+++ home-manager/desktop/default.nix
@@ -114,7 +114,7 @@
       #ms-vscode-remote.remote-containers
       ms-vscode-remote.remote-ssh
       oderwat.indent-rainbow
-      redhat.java
+      #redhat.java
       rust-lang.rust-analyzer
       yzhang.markdown-all-in-one
     ];
