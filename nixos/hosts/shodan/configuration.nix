983e994930b9e023991980c7069e5f02acc9733f
Lillian-Violet <git@lillianviolet.dev>
2024-02-26 12:59:49 +0100

    Remove some unneeded comments

--- nixos/hosts/shodan/configuration.nix
+++ nixos/hosts/shodan/configuration.nix
@@ -32,16 +32,6 @@
     # You can add overlays here
     overlays = [
       inputs.extest.overlays.default
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
     # Configure your nixpkgs instance
     config = {
