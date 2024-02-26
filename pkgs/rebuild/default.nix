0d03dc90a73ac533f69256ee8a45505fa324c910
Lillian-Violet <git@lillianviolet.dev>
2024-02-26 14:23:12 +0100

    The logging doesn't work, let's remove it

--- pkgs/rebuild/default.nix
+++ pkgs/rebuild/default.nix
@@ -16,7 +16,7 @@ writeShellApplication
     git clone forgejo@git.lillianviolet.dev:Lillian-Violet/NixOS-Config.git ./rebuild
     pushd ./rebuild
     echo "NixOS Rebuilding..."
-    sudo nixos-rebuild switch --flake .# &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
+    sudo nixos-rebuild switch --flake .#
     popd
     echo "Cleaning up repository in tmp..."
     rm -rf ./rebuild
