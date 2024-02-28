6af277d0aff28f88fd86f763b572289fb1a36606
Lillian-Violet <git@lillianviolet.dev>
2024-02-28 11:01:48 +0100

    Add fancy selection menu to install bash app

--- pkgs/install-nix/default.nix
+++ pkgs/install-nix/default.nix
@@ -12,16 +12,20 @@ writeShellApplication
   text = ''
     # An install script for NixOS installation to /tmp
     set -e
-    hostname=$1
-    if [[ "$1" == "" ]]; then
-      echo "No hostname given, please specify a hostname"
-      exit 2
-    fi
     pushd /tmp
     git clone https://git.lillianviolet.dev/Lillian-Violet/NixOS-Config.git ./install
+    pushd ./install/nixos/hosts
+    echo "Please choose the hostname you are installing to from the following list:"
+    i=1
+    for d in */
+    do
+      dirs[i++]="$\{d%/}"
+    done
+    select dir in "$\{dirs[@]}"; do echo "you selected $\{dir}"; break; done
+    popd
     pushd ./install
     echo "NixOS Installing..."
-    sudo nixos-install --flake .#$1
+    sudo nixos-install --flake .#$dir
     popd
     echo "Cleaning up repository in tmp..."
     rm -rf ./install
