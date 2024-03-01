1878f7b4497cf38b5e055de079865506878f57e7
Lillian-Violet <git@lillianviolet.dev>
2024-03-01 15:36:38 +0100

    Let's try only absolute paths then

--- pkgs/systemd-rebuild/default.nix
+++ pkgs/systemd-rebuild/default.nix
@@ -11,15 +11,12 @@ writeShellApplication
 
   text = ''
     # A rebuild script for NixOS for use of the systemd service
-    cd /tmp
-    rm -rf ./rebuild
-    systemd-inhibit git clone https://git.lillianviolet.dev/Lillian-Violet/NixOS-Config.git ./rebuild
-    cd ./rebuild
-    echo "NixOS Rebuilding..."
-    systemd-inhibit nixos-rebuild switch --flake .#
-    echo "Cleaning up repository in '/tmp/rebuild'..."
-    cd ..
-    systemd-inhibit rm -rf ./rebuild
-    echo "NixOS Rebuilt OK!"
+    systemd-inhibit rm -rf /tmp/rebuild
+    systemd-inhibit git clone https://git.lillianviolet.dev/Lillian-Violet/NixOS-Config.git /tmp/rebuild
+    systemd-inhibit echo "NixOS Rebuilding..."
+    systemd-inhibit nixos-rebuild switch --flake /tmp/rebuild/#
+    systemd-inhibit echo "Cleaning up repository in '/tmp/rebuild'..."
+    systemd-inhibit rm -rf /tmp/rebuild
+    systemd-inhibit echo "NixOS Rebuilt OK!"
   '';
 }
