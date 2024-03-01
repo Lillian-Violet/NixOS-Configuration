ace01f3391c22faff15676622c4b2e9bd23e51a4
Lillian-Violet <git@lillianviolet.dev>
2024-03-01 16:18:10 +0100

    That doesn't seem to work as well for some reason

--- pkgs/rebuild/default.nix
+++ pkgs/rebuild/default.nix
@@ -12,8 +12,16 @@ writeShellApplication
   text = ''
     # A rebuild script for NixOS
     set -e
+    pushd /tmp > /dev/null
+    rm -rf ./rebuild
+    systemd-inhibit --what=idle git clone https://git.lillianviolet.dev/Lillian-Violet/NixOS-Config.git ./rebuild
+    pushd ./rebuild > /dev/null
     echo "NixOS Rebuilding..."
-    sudo systemd-inhibit --who="System Updater" --why="Updating System" nixos-rebuild switch --flake git+https://git.lillianviolet.dev/Lillian-Violet/NixOS-Config.git#
+    systemd-inhibit --what=idle sudo nixos-rebuild switch --flake .#
+    popd > /dev/null
+    echo "Cleaning up repository in '/tmp/rebuild'..."
+    systemd-inhibit --what=idle rm -rf ./rebuild
+    popd > /dev/null
     echo "NixOS Rebuilt OK!"
   '';
 }
