c236a32fe4b3d742c9956f5edde2cea6d42725bf
Lillian-Violet <git@lillianviolet.dev>
2024-03-12 14:57:59 +0100

    Cannot update with sudo since it doesn't have the ssh key

--- pkgs/upgrade/default.nix
+++ pkgs/upgrade/default.nix
@@ -11,7 +11,7 @@ writeShellApplication
 
   text = ''
     # An upgrade script for nixos
-    sudo systemd-inhibit --who="NixOS Updater" --why="Updating flake lock" update
+    update
     sudo systemd-inhibit --who="NixOS Updater" --why="Updating system configuration" rebuild-no-inhibit
   '';
 }
