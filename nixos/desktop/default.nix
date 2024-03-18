8dc0ceb16bdbe873257303f23c62031c614832f1
Lillian-Violet <git@lillianviolet.dev>
2024-03-18 16:06:23 +0100

    Run SDDM on wayland

--- nixos/desktop/default.nix
+++ nixos/desktop/default.nix
@@ -89,7 +89,7 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
   services.xserver.displayManager.defaultSession = "plasma";
   programs.kdeconnect.enable = true;
