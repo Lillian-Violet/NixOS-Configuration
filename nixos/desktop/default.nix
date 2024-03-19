c38a2b6f66ac9062c69f10b90d49cd02ab9e0971
Lillian-Violet <git@lillianviolet.dev>
2024-03-19 12:18:16 +0100

    Enable sddm

--- nixos/desktop/default.nix
+++ nixos/desktop/default.nix
@@ -93,6 +93,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.wayland.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
   services.xserver.displayManager.defaultSession = "plasma";
   programs.kdeconnect.enable = true;
