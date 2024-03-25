4a34a1147414a71049b2f39c7cce002f8288d9c8
Lillian-Violet <git@lillianviolet.dev>
2024-03-25 18:28:07 +0100

    Let's try forcing patched steam

--- nixos/hosts/shodan/configuration.nix
+++ nixos/hosts/shodan/configuration.nix
@@ -121,7 +121,7 @@
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    package = pkgs.steam.override {
+    package = lib.mkForce pkgs.steam.override {
       extraProfile = ''export LD_PRELOAD=${pkgs.extest}/lib/libextest.so:$LD_PRELOAD'';
     };
   };
