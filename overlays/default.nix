08d443f5c31575657e2673a9a1bc0718ba2f0550
Lillian-Violet <git@lillianviolet.dev>
2024-03-25 18:51:21 +0100

    Let's try a global overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,20 +1,20 @@
 # This file defines overlays
 {inputs, ...}: {
+  # This one contains whatever you want to overlay
+  # You can change versions, add patches, set compilation flags, anything really.
+  # https://nixos.wiki/wiki/Overlays
+  modifications = final: prev: {
+    steam = prev.steam.override {
+      extraProfile = ''export LD_PRELOAD=${inputs.extest}/lib/libextest.so:$LD_PRELOAD'';
+    };
+  };
+
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev:
     import ../pkgs {
       inherit (final) callPackage;
       pkgs = final;
     };
-
-  # This one contains whatever you want to overlay
-  # You can change versions, add patches, set compilation flags, anything really.
-  # https://nixos.wiki/wiki/Overlays
-  modifications = final: prev: {
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
-  };
 
   # When applied, the stable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.stable'
