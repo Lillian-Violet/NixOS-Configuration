740e79cc9f94539a30d570c0cc57476b6ef7562c
Lillian-Violet <git@lillianviolet.dev>
2024-03-28 11:12:02 +0100

    Add telegraf logging to queen (hardware logging)

--- nixos/server/package-configs/dashboard/default.nix
+++ nixos/server/package-configs/dashboard/default.nix
@@ -10,5 +10,6 @@
     ./grafana
     #./loki
     ./prometheus
+    ./telegraf
   ];
 }
---
+++ nixos/server/package-configs/dashboard/telegraf/default.nix
@@ -0,0 +1,53 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  user,
+  ...
+}: {
+  services.telegraf = {
+    enable = true;
+    extraConfig = {
+      agent = {
+        interval = "10s";
+        round_interval = true;
+        metric_batch_size = 1000;
+        metric_buffer_limit = 10000;
+        collection_jitter = "0s";
+        flush_interval = "10s";
+        flush_jitter = "0s";
+        precision = "";
+        debug = false;
+        quiet = false;
+        logfile = "";
+        hostname = "queen";
+        omit_hostname = false;
+      };
+      inputs = {
+        cpu = {
+          percpu = true;
+          totalcpu = true;
+          collect_cpu_time = false;
+          report_active = false;
+          core_tags = false;
+        };
+        disk = {
+          ignore_fs = ["tmpfs" "devtmpfs" "devfs" "overlay" "aufs" "squashfs"];
+        };
+        diskio = {};
+        kernel = {};
+        mem = {};
+        system = {};
+      };
+      # outputs = {
+      #   influxdb_v2 = {
+      #     urls = ["http://monitoringvm:8086"];
+      #     token = "\${influx_token}";
+      #     organization = "home";
+      #     bucket = "tigstack";
+      #   };
+      # };
+    };
+  };
+}
